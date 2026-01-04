eedd34486b601d04ad7f9cd4418044649aae59ea
justin <schoenakerjustin@gmail.com>
2026-01-04 15:00:50 +0100

    Innitial push

---
+++ __main__.py
@@ -0,0 +1,27 @@
+"""A Python Pulumi program"""
+
+import pulumi
+import requests
+import time
+
+config = pulumi.Config()
+vault_addr = config.require("vault:address")
+
+def wait_for_vault(url: str, timeout=60):
+    pulumi.log.info(f"Checking Vault health at {url}")
+    start = time.time()
+
+    while time.time() - start < timeout:
+        try:
+            r = requests.get(f"{url}/v1/sys/health", timeout=2)
+            if r.status_code in (200, 429, 472):
+                pulumi.log.info("Vault is reachable")
+                return True
+        except Exception:
+            pass
+
+        time.sleep(2)
+
+    raise Exception("Vault did not become ready in time")
+
+wait_for_vault(vault_addr)
